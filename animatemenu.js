ca88fcc67dd26ad5622f10cf0bdb80f467f218f3
amarb16 <Stults@ariels-mbp-2.coldspring.marian.edu>
2016-06-30 11:20:16 -0400

    Still working on it

---
+++ animatemenu.js
@@ -0,0 +1,10 @@
+var main = function() {
+  $('.menupic').click(function() {
+    $(this).toggleClass();
+  //   $('body').animate({
+  //       left: '-285px'
+  //       }, 200);
+  // });
+};
+
+$(document).ready(main);
---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAjsMd7uYjK0jWJUHidaQdrJXEyGL4wgc4",
+    authDomain: "grocerylist-e67b5.firebaseapp.com",
+    databaseURL: "https://grocerylist-e67b5.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
